bc58a1aa4cf8c7e739a37bae9770d86f1d396eb6
Tim Bryant <timcalvin@gmail.com>
2024-03-22 12:17:34 -0400

    Initial Commit

---
+++ ServiceTracker/ServiceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceTrackerApp.swift
+//  ServiceTracker
+//
+//  Created by Timothy Bryant on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ServiceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
